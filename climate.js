08c8f6fd92ec22d481c8430e5b84b50eae412576
RiA <105122539+glorianwachukwu1@users.noreply.github.com>
2023-12-03 06:16:34 -0500

    Update climate.js

--- climate.js
+++ climate.js
@@ -5,11 +5,25 @@ document.addEventListener('DOMContentLoaded', function () {
     const getWeatherResultContainer = document.getElementById('getWeatherResultContainer');
     const searchResultContainer = document.getElementById('searchResultContainer');
 
+ // Function to reset the visibility of result containers
+ function resetResultContainers() {
+    getWeatherResultContainer.style.display = 'none';
+    searchResultContainer.style.display = 'none';
+}
+
+// Call the reset function when the page is loaded
+resetResultContainers();
+
+
     getWeatherButton.addEventListener('click', function () {
-               getWeatherResultContainer.style.display = getWeatherResultContainer.style.display === 'none' ? 'block' : 'none';
+        // Toggle the visibility of the get weather result container
+        getWeatherResultContainer.style.display = getWeatherResultContainer.style.display === 'none' ? 'block' : 'none';
     });
 
     searchButton.addEventListener('click', function () {
-                searchResultContainer.style.display = searchResultContainer.style.display === 'none' ? 'block' : 'none';
+        // Toggle the visibility of the search result container
+        searchResultContainer.style.display = searchResultContainer.style.display === 'none' ? 'block' : 'none';
+    
+  
     });
 });
